7dd27e6485c08c5e6b135f0b81818bff2708501b
sakshikamal <sakshikamal99@gmail.com>
2021-07-08 12:21:59 +0530

    added flask backend

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask_ngrok import run_with_ngrok
+from flask import Flask, render_template
+
+app=Flask(__name__)
+run_with_ngrok(app)
+
+@app.route('/')
+def hello_world():
+  return render_template('dashboard.html')
+
+if __name__ == '__main__':
+  app.run()
