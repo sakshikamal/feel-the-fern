ebe7d2f81a461ec1d821f2fc8aae4037403791fc
sakshikamal <sakshikamal99@gmail.com>
2021-07-10 01:44:27 +0530

    added routes

--- app.py
+++ app.py
@@ -5,8 +5,24 @@ app=Flask(__name__)
 run_with_ngrok(app)
 
 @app.route('/')
-def hello_world():
+def display_dashboard():
   return render_template('dashboard.html')
+
+@app.route('/user')
+def display_user():
+  return render_template('user.html')
+
+@app.route('/appointments')
+def display_appointment():
+  return render_template('tables.html')
+
+@app.route('/map')
+def display_user():
+  return render_template('maps.html')
+
+@app.route('/notifications')
+def display_notifications():
+  return render_template('notifications.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
